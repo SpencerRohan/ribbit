35afb609d8dc6beffdf8f982a7c9be72264ecd78
Spencer Rohan <spencermrohan@gmail.com>
2015-10-08 07:49:39 -0500

    initialize test

--- spec/ribbit_spec.rb
+++ spec/ribbit_spec.rb
@@ -1,1 +1,14 @@
 require_relative "../lib/ribbit.rb"
+
+describe Ribbit do
+  before :each do
+    @ribbit = Ribbit.new(income: 50000, zipcode: 60201, age: 35)
+  end
+
+  describe "#initialize" do
+    it "takes a hash argument and returns a Ribbit object" do
+      @ribbit.to be_a(Ribbit)
+    end
+  end
+
+end
