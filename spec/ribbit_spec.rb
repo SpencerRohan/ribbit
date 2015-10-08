6a47eb649ec728a754ae2aa151286b611ef3c59c
Spencer Rohan <spencermrohan@gmail.com>
2015-10-08 08:43:18 -0500

    add request builder

--- spec/ribbit_spec.rb
+++ spec/ribbit_spec.rb
@@ -10,16 +10,26 @@ describe Ribbit do
       expect(@ribbit).to be_a(Ribbit)
     end
 
-    it "Makes income a readable value" do
+    it "Makes #income a readable value" do
       expect(@ribbit.income).to eq(50000)
     end
 
-    it "Makes zipcode a readable value" do
+    it "Makes #zipcode a readable value" do
       expect(@ribbit.zipcode).to eq(60201)
     end
 
-    it "Makes age a readable value" do
+    it "Makes #age a readable value" do
       expect(@ribbit.age).to eq(35)
+    end
+  end
+
+  describe "#request" do
+    it "returns a string" do
+      expect(@ribbit.request).to be_a(String)
+    end
+
+    it "returns a url using consumer data as params" do
+      expect(@ribbit.request).to eq("http://not_real.com/customer_scoring?income=50000&zipcode=60201&age=35")
     end
   end
 
