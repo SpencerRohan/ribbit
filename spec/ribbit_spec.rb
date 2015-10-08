5eecc858b5bcedd8c48aeb787d21dbccfb98a397
Spencer Rohan <spencermrohan@gmail.com>
2015-10-08 12:00:55 -0500

    add edge case testing for bad conusumer data

--- spec/ribbit_spec.rb
+++ spec/ribbit_spec.rb
@@ -2,35 +2,45 @@ require_relative "../lib/ribbit.rb"
 require 'webmock/rspec'
 
 describe Ribbit do
-  before :each do
-    @ribbit = Ribbit.new(income: 50000, zipcode: 60201, age: 35)
-  end
+  let(:ribbit) { Ribbit.new(income: 50000, zipcode: 60201, age: 35) }
+  let(:badribbit1) { Ribbit.new(income: "50000", age: "35") }
+  let(:badribbit2) { Ribbit.new(income: "50000", zipcode: 60201, age: 35) }
 
   describe "#initialize" do
     it "takes a hash argument and returns a Ribbit object" do
-      expect(@ribbit).to be_a(Ribbit)
+      expect(ribbit).to be_a(Ribbit)
     end
 
     it "Makes #income a readable value" do
-      expect(@ribbit.income).to eq(50000)
+      expect(ribbit.income).to eq(50000)
     end
 
+
     it "Makes #zipcode a readable value" do
-      expect(@ribbit.zipcode).to eq(60201)
+      expect(ribbit.zipcode).to eq(60201)
     end
 
     it "Makes #age a readable value" do
-      expect(@ribbit.age).to eq(35)
+      expect(ribbit.age).to eq(35)
     end
+
+    it "raises errors if any consumer data is missing" do
+      expect{ badribbit1 }.to raise_error(ArgumentError)
+    end
+
+    it "raises errors if any consumer data is not a fixnum" do
+      expect{ badribbit2 }.to raise_error(ArgumentError)
+    end
+
   end
 
   describe "#destination" do
     it "returns a string" do
-      expect(@ribbit.destination).to be_a(String)
+      expect(ribbit.destination).to be_a(String)
     end
 
     it "returns a url using consumer data as params" do
-      expect(@ribbit.destination).to eq("http://notreal.com/customerscoring?income=50000&zipcode=60201&age=35")
+      expect(ribbit.destination).to eq("http://notreal.com/customerscoring?income=50000&zipcode=60201&age=35")
     end
   end
 
@@ -39,7 +49,7 @@ describe Ribbit do
       WebMock.stub_request(:get, 'http://notreal.com/customerscoring?age=35&income=50000&zipcode=60201').to_return(:body   => {propensity: 0.26532,
                               ranking: 'C'}.to_json,
                   :status => 200)
-      expect(@ribbit.request).to eq({ "propensity" => 0.26532,
+      expect(ribbit.request).to eq({ "propensity" => 0.26532,
                                       "ranking"    => "C"})
     end
 
@@ -47,7 +57,7 @@ describe Ribbit do
       WebMock.stub_request(:get, 'http://notreal.com/customerscoring?age=35&income=50000&zipcode=60201').to_return(:body   => {propensity: 0.26532,
                               ranking: 'C'}.to_json,
                   :status => 404)
-      expect{ @ribbit.request }.to raise_error(ArgumentError)
+      expect{ ribbit.request }.to raise_error(ArgumentError)
     end
   end
 
